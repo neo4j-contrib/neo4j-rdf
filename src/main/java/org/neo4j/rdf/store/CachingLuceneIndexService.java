6a709477a169379948dc5f3b5b764aea58662f99
Mattias Persson <mattias@neotechnology.com>
2008-05-12 11:00:37 +0000

    Uses lucene index service with caching on "uri" property

---
+++ src/main/java/org/neo4j/rdf/store/CachingLuceneIndexService.java
@@ -0,0 +1,15 @@
+package org.neo4j.rdf.store;
+
+import org.neo4j.api.core.NeoService;
+import org.neo4j.rdf.store.representation.standard.AbstractUriBasedExecutor;
+import org.neo4j.util.index.LuceneIndexService;
+
+public class CachingLuceneIndexService extends LuceneIndexService
+{
+	public CachingLuceneIndexService( NeoService neo )
+    {
+	    super( neo );
+	    enableCache( AbstractUriBasedExecutor.URI_PROPERTY_KEY,
+	    	100000 );
+    }
+}
